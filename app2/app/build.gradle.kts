b68807487865086b4b763b8fe1ad0bff3de248ba
kikiarajun <cryptojun@gmail.com>
2024-09-06 17:39:57 +0900

    commit to github3

--- app2/app/build.gradle.kts
+++ app2/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.app2"
-        minSdk = 33
-        targetSdk = 33
+        minSdk = 28
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
